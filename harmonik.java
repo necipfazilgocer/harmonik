0845c74ab7cfb2a02c05d76a8886bcb797e13772
necipfazilgocer <114177524+necipfazilgocer@users.noreply.github.com>
2022-09-26 15:33:58 +0300

    Add files via upload

---
+++ harmonik.java
@@ -0,0 +1,19 @@
+package harmonik;
+import java.util.Scanner;
+public class harmonik {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Bir sayı giriniz:");
+		int sayi=scanner.nextInt();
+		double sonuc=0;
+		double a=1;
+		for(int i=1;i<=sayi;i++)
+		{
+			
+			sonuc+=(a/i);
+		}
+		System.out.println(sonuc);
+	}
+
+}
